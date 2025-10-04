028d8f23259527524f8a68744eb3b118d71748eb
dianannperla <waafirmelyza@hotmail.com>
2025-10-04 18:17:32 +0100

    Update output.filename to include a hash for cache busting.

--- webpack.config.js
+++ webpack.config.js
@@ -3,14 +3,14 @@ const path = require('path');
 module.exports = {
     entry: './src/index.js',
     output: {
-        filename: 'bundle.js',
+        filename: 'bundle.[contenthash].js',
         path: path.resolve(__dirname, 'dist'),
         publicPath: '/'  // Set the publicPath to root for proper asset loading
     },
     module: {
         rules: [
             {
-                test: /.jsx?$/, 
+                test: /.jsx?$/,
                 exclude: /node_modules/, 
                 use: {
                     loader: 'babel-loader',
