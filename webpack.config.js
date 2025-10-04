5ec6b7335926fe37db8cd37b368237f9bbeb5103
dianannperla <waafirmelyza@hotmail.com>
2025-10-04 18:12:49 +0100

    Update publicPath to use Node.js path resolver for better compatibility

--- webpack.config.js
+++ webpack.config.js
@@ -5,12 +5,12 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
-        publicPath: "/"
+        publicPath: path.resolve(__dirname, '/')
     },
     module: {
         rules: [
             {
-                test: /\.jsx?$/,
+                test: /\.jsx?$/, 
                 exclude: /node_modules/, 
                 use: {
                     loader: 'babel-loader',
