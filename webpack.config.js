c592be0c6769701b2e0038765a26b32526f669ef
dianannperla <waafirmelyza@hotmail.com>
2025-10-04 18:13:31 +0100

    Fix publicPath in webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
-        publicPath: path.resolve(__dirname, '/')
+        publicPath: '/'  // Updated publicPath to be a string instead of resolving
     },
     module: {
         rules: [
